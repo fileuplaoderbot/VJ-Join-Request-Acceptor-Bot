c494d2231ad86e0be1ad43baa78f5e86468d941f
fileuplaoderbot <tparveen2728@gmail.com>
2025-01-20 01:48:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002335115531"))
 ADMINS = int(environ.get("ADMINS", "6069621485"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_URI = environ.get("DB_URI", "mongodb+srv://AniReal123:AniReal123@anireal123.wqnsp.mongodb.net/?retryWrites=true&w=majority&appName=AniReal123") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = environ.get("DB_NAME", "AniReal123")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
