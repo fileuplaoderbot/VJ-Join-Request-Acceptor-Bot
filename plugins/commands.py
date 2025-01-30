b1f743aa0a2c01f2159ffc81d0bad8bfe26171af
fileuplaoderbot <tparveen2728@gmail.com>
2025-01-31 01:51:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,7 @@ async def start_message(c,m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id, m.from_user.first_name)
         await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://envs.sh/a6r.jpg",
+    await m.reply_photo(f"https://envs.sh/a-6.jpg",
         caption=f"<b>Hello {m.from_user.mention} 👋\n\nI Am Join Request Acceptor Bot. I Can Accept All Old Pending Join Request.\n\nFor All Pending Join Request Use - /accept</b>",
         reply_markup=InlineKeyboardMarkup(
             [[
